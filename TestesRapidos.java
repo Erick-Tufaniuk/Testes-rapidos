0c7d363950db9b37d8c36e26011506d8b132f5bf
Erick-Tufaniuk <octoniuk@gmail.com>
2024-05-27 11:15:28 -0300

    update

--- TestesRapidos.java
+++ TestesRapidos.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class TestesRapidos{
     public static void main(String[]aargs){
+        Scanner sc = new Scanner(System.in);
+
+        String[] nomes = new String[4];
         
+        System.out.println("Digite o primeiro nome: ");
+        nomes[0] = sc.nextLine();
+        System.out.println("Digite o segundo nome: ");
+        nomes[1] = sc.nextLine();
+        System.out.println("Digite o terceiro nome: ");
+        nomes[2] = sc.nextLine();
+        System.out.println("Digite o quarto nome: ");
+        nomes[3] = sc.nextLine();
+
+       for(int x =0; x< nomes.length; x++){
+        System.out.println(nomes[x]);
+       }
     }
 }
